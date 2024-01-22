1b7e39d374224b05efd95387fd74ea05b4e6247e
Tin Wongcharoo <Tinn.Wongcharoo@gmail.com>
2024-01-22 21:06:02 +0700

    add nev bar

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Nevbar from '@/components/Nevbar'
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <Nevbar/>
+      <iframe src="https://drive.google.com/file/d/1GvOMRwJMtzOZ2jb7zJPcby2N6iyHd0Ft/preview" width="640" height="480" allow="autoplay"></iframe>
+    </main>
+  )
+}
---
+++ portfolio/src/components/Nevbar/index.tsx
@@ -0,0 +1,16 @@
+
+function Nevbar() {
+  return (
+    <div className="w-full p-12">
+      <div className="flex justify-between sm:px-12">
+        <h1>TJ7</h1>
+        <div className="flex flex-row gap-5 sm:gap-10">
+          <h1>Project</h1>
+          <h1>About</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nevbar
