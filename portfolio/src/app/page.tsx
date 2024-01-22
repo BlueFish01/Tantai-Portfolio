e88f75f2f3394f387f6b635c56b273c065a76b20
Tin Wongcharoo <Tinn.Wongcharoo@gmail.com>
2024-01-22 22:24:30 +0700

    "add basic navigation to nev bar"

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,10 +2,13 @@ import Nevbar from '@/components/Nevbar'
 
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <Nevbar/>
-      <iframe src="https://drive.google.com/file/d/1GvOMRwJMtzOZ2jb7zJPcby2N6iyHd0Ft/preview" width="640" height="480" allow="autoplay"></iframe>
+      
+      <video id="bgVideo" preload="true" autoPlay loop muted>
+        <source src={"Huracan_STO.mp4"} type='video/mp4'/>
+      </video>
     </main>
   )
 }
--- portfolio/src/components/Nevbar/index.tsx
+++ portfolio/src/components/Nevbar/index.tsx
@@ -1,12 +1,28 @@
+import Link from 'next/link'
 
 function Nevbar() {
+
+  const pages = {
+    Project : "/project",
+    About : "/about"
+  }
+
+  const homePath = '/'
+
   return (
-    <div className="w-full p-12">
-      <div className="flex justify-between sm:px-12">
-        <h1>TJ7</h1>
+    <div className="w-full py-8 px-12">
+      <div className="flex justify-between sm:px-10">
+        <Link href={homePath}>
+          <h1 className="text-xl font-bold">
+            TJ7
+          </h1>
+        </Link>
         <div className="flex flex-row gap-5 sm:gap-10">
-          <h1>Project</h1>
-          <h1>About</h1>
+          {Object.entries(pages).map(([page,path])=>(
+            <Link href={path}>
+                {page}
+            </Link>
+          ))}
         </div>
       </div>
     </div>
