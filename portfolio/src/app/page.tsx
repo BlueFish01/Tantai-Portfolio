74c30cbf354dc2f3a5bffd2c85f217378668b4bd
Tin Wongcharoo <Tinn.Wongcharoo@gmail.com>
2024-01-29 22:04:37 +0700

    fix backgrond video size to fit on screen

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,14 +1,35 @@
-import Nevbar from '@/components/Nevbar'
+import Image from 'next/image';
 
 
 export default function Home() {
-
   return (
     <main className="flex min-h-screen flex-col items-center">
-      
-      <video id="bgVideo" preload="true" autoPlay loop muted>
-        <source src={"Huracan_STO.mp4"} type='video/mp4'/>
-      </video>
+      <div className="flex flex-col justify-center items-center">
+        <video
+          id="bgVideo"
+          preload="true"
+          autoPlay
+          loop
+          muted
+          className="w-full h-screen object-cover sm:top-0 left-0 z-[-1]"
+        >
+          <source src={"Huracan_STO.mp4"} type="video/mp4" />
+        </video>
+          <h1 className="width-50 py-20">
+            Brand I've worked with
+          </h1>
+        <div>
+        
+          <Image
+            src={"aston-martin-logo.svg"}
+            alt="My SVG"
+            width={200}
+            height={200}
+          />
+    
+
+        </div>
+      </div>
     </main>
-  )
+  );
 }
